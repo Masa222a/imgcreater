151ceccd369c8f2d67839b318272325ab4319b4c
Masa222a <guitarstring0716@gmail.com>
2023-05-17 14:50:14 +0900

    navigationの導入

--- app/src/main/java/com/example/imgcreater/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/imgcreater/view/activity/MainActivity.kt
@@ -2,12 +2,41 @@ package com.example.imgcreater.view.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.imgcreater.R
+import com.example.imgcreater.databinding.ActivityMainBinding
+import com.example.imgcreater.view.fragment.HistoryFragment
+import com.example.imgcreater.view.fragment.MainFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNav = binding.bottomNavigation
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.main -> {
+                    loadFragment(MainFragment())
+                    true
+                }
+                else -> {
+                    loadFragment(HistoryFragment())
+                    true
+                }
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.apply {
+            replace(R.id.container, fragment)
+            commit()
+        }
+
     }
 }
