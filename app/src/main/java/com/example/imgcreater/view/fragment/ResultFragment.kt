16d248930c609aa5d0cac86a24ce05b41951be5d
Masa222a <guitarstring0716@gmail.com>
2023-05-22 12:30:02 +0900

    Result画面のonCreateViewにあるnull許容型を修正

--- app/src/main/java/com/example/imgcreater/view/fragment/ResultFragment.kt
+++ app/src/main/java/com/example/imgcreater/view/fragment/ResultFragment.kt
@@ -15,7 +15,7 @@ class ResultFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
         binding = FragmentResultBinding.inflate(inflater, container, false)
 
         binding.backButton.setOnClickListener(
