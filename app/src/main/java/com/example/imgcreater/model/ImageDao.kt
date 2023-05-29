95a2e00f18b47993a3d98a07341458086ff09757
Masa222a <guitarstring0716@gmail.com>
2023-05-29 14:24:52 +0900

    Daoファイルを作成

---
+++ app/src/main/java/com/example/imgcreater/model/ImageDao.kt
@@ -0,0 +1,19 @@
+package com.example.imgcreater.model
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface ImageDao {
+    @Insert
+    fun insertImage(imageModel: ImageEntity)
+
+    @Delete
+    fun deleteImage(imageModel: ImageEntity)
+
+    @Query("SELECT * FROM image_data_table")
+    fun getAllImages(): LiveData<List<ImageEntity>>
+}
