24dce7f57bd2880a3a8f3097440d58819fe17cee
Masa222a <guitarstring0716@gmail.com>
2023-05-29 14:25:12 +0900

    Repositoryの作成

---
+++ app/src/main/java/com/example/imgcreater/repository/ImageRepository.kt
@@ -0,0 +1,16 @@
+package com.example.imgcreater.repository
+
+import androidx.lifecycle.LiveData
+import com.example.imgcreater.model.ImageDao
+import com.example.imgcreater.model.ImageEntity
+
+class ImageRepository(private val imageDao: ImageDao) {
+
+    val allImages: LiveData<List<ImageEntity>> = imageDao.getAllImages()
+
+    suspend fun insertImages(imageModel: ImageEntity) =
+        imageDao.insertImage(imageModel)
+
+    suspend fun deleteImages(imageModel: ImageEntity) =
+        imageDao.deleteImage(imageModel)
+}
