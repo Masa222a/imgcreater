0ee6e67f833f6e2995c985532a18c1e00b2eb671
Masa222a <guitarstring0716@gmail.com>
2023-05-31 22:44:27 +0900

    保存ボタンを押した際に画像を保存する

--- app/src/main/java/com/example/imgcreater/view/fragment/ResultFragment.kt
+++ app/src/main/java/com/example/imgcreater/view/fragment/ResultFragment.kt
@@ -1,19 +1,23 @@
 package com.example.imgcreater.view.fragment
 
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import com.example.imgcreater.R
 import com.example.imgcreater.databinding.FragmentResultBinding
+import com.example.imgcreater.viewmodel.ResultViewModel
 import com.squareup.picasso.Picasso
 
 class ResultFragment : Fragment() {
     private lateinit var binding: FragmentResultBinding
     private val args: MainFragmentArgs by navArgs()
+    private val viewModel: ResultViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +31,15 @@ class ResultFragment : Fragment() {
             Navigation.createNavigateOnClickListener(R.id.action_resultFragment_to_nav_main)
         )
 
+        binding.saveButton.setOnClickListener {
+            val image = (binding.resultImg.drawable as BitmapDrawable).bitmap
+            val contentResolver = requireContext().contentResolver
+            viewModel.apply {
+                imageData.postValue(image)
+                saveImageToStrage(image, contentResolver)
+            }
+        }
+
         return binding.root
     }
-
 }
